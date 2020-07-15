a71e50dbf261a696abfdc5ecb981194a5a9b755d
Sylvia <sylviaprabudy@gmail.com>
2020-07-15 17:15:48 -0500

    add a question with inquirer

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const profileDataArgs = process.argv.slice(2);
+
+// const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+.prompt([
+    {
+        typw: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
