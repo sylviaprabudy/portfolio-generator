1ad43f6046cf1559abf810a4f94ebab9a530bd2e
Sylvia <sylviaprabudy@gmail.com>
2020-07-14 22:17:47 -0500

    capture user input

---
+++ app.js
@@ -0,0 +1,9 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+    }
+};
+printProfileData(profileDataArgs)
