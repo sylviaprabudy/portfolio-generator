3a4337e61f832e9384427c66a0eabced441edc27
Sylvia <sylviaprabudy@gmail.com>
2020-07-15 00:09:34 -0500

    generate HTML file

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-    }
-};
-printProfileData(profileDataArgs)
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
